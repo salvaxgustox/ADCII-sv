31371fd0cd20999b2122ada3216af1afb9a977d5
salvaxgustox <august_182@hotmail.com>
2022-06-03 01:24:11 -0600

    Adding post and get controller

--- ADCII-sv-controller.cs
+++ ADCII-sv-controller.cs
@@ -1,35 +1,73 @@
 using System;
-using System.IO;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace umg.salva
 {
     public static class ADCII_sv_controller
     {
         [FunctionName("ADCII_sv_controller")]
-        public static async Task<IActionResult> Run(
+        public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            string SqlConnectionString ="Server=tcp:umg.database.windows.net,1433;Initial Catalog=project;Persist Security Info=False;User ID=dbasalva;Password=Salva301190!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            SqlConnection conn = new SqlConnection(SqlConnectionString);
+            conn.Open();
+            string result = "...";
+            log.LogInformation($"C# HTTP trigger function processed a request {req.Method}.");
+            
+            if (HttpMethods.IsPost(req.Method))
+            {
+                bool bomba = Boolean.Parse(req.Query["b"]);
+                bool sensor = Boolean.Parse(req.Query["s"]);
+                bool llave = Boolean.Parse(req.Query["l"]);
+                string sql =  "INSERT INTO controller(bomba,sensor,llave,fecha) VALUES(@param1,@param2,@param3,@param4)";
+                Console.WriteLine(sql);
+                using(SqlCommand cmd = new SqlCommand(sql,conn)) 
+                {
+                    try
+                    {
+                        cmd.Parameters.Add("@param1", SqlDbType.Bit).Value = bomba;
+                        cmd.Parameters.Add("@param2", SqlDbType.Bit).Value = sensor;
+                        cmd.Parameters.Add("@param3", SqlDbType.Bit).Value = llave;
+                        cmd.Parameters.Add("@param4", SqlDbType.DateTime).Value = DateTime.Now;
+                        cmd.CommandType = CommandType.Text;
+                        int query_rows_affected = cmd.ExecuteNonQuery();
+                        Console.WriteLine($"Rows affected: {query_rows_affected}");
+                        result = "ok";
+                    }
+                    catch (System.Exception)
+                    {
+                        result = "Error en base de datos";
+                    }
+                    
+                }
+            } else if (HttpMethods.IsGet(req.Method)){
+                string sql_command = "Select * from [dbo].[controller] order by fecha desc";
+                    Console.WriteLine($"Sql command to be executed: {sql_command}");
+                    SqlCommand command = new SqlCommand(sql_command, conn);
+                    command.CommandType = System.Data.CommandType.Text;
+                    using (SqlDataReader reader = command.ExecuteReader()) {
+                        if (reader.Read()) {
+                            Console.WriteLine($"Successfully Executed Azure Function at: {DateTime.Now}");
+                            log.LogInformation($"Successfully Executed Azure Function at: {DateTime.Now}");
+                            Console.WriteLine($"Row count processed: "+ reader.GetBoolean(1).ToString());
+                            log.LogInformation($"Row count processed: "+ reader.GetBoolean(1).ToString());
+                            result = $"{{b: {reader.GetBoolean(1)}, s: {reader.GetBoolean(2)}, l: {reader.GetBoolean(3)}, fecha: {reader.GetDateTime(4)} }}";
+                        }
+                    }
+            }
+            
+            
+            
+            conn.Close();
+            return new OkObjectResult(result);
         }
     }
 }
